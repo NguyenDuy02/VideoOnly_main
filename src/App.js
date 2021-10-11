4080c35dbf4f8130977ea9ca91f9ab76ad2b421a
NguyenDuy02 <nguyendinhduy020396@gmai.com>
2021-10-11 12:45:53 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import "./App.scss";
 import "./assets/css/styles.scss";
 import "./assets/css/responsive.scss";
+import "./App.scss";
 import "./assets/fonts/AdobeGothicStd-Bold.otf";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import {Header} from './components/Header';
